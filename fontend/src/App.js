6ab8866456c6be688131b54e539bfef7dfc2a8a7
jeansn <jeansn@tfg.co.za>
2020-10-30 16:22:08 +0200

    added header comp

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,8 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Hello React</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
