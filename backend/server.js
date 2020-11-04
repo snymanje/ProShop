0cfe75bf145ac3ff416e785b41eab855e6d25f38
jeansn <jeansn@tfg.co.za>
2020-11-04 06:27:19 +0200

    added login logic

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,18 @@ import dotenv from "dotenv";
 import "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 connectDB();
 
 app.use("/api/products/", productRoutes);
+app.use("/api/users/", userRoutes);
 
 app.use(notFound);
 
