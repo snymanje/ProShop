2ca62811ebb3c6a395917120730d2919f00923c3
jeansn <jeansn@tfg.co.za>
2020-11-08 07:15:19 +0200

    order logic done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import { userLoginReducer } from "./reducers/userReducers";
 import { userRegisterReducer } from "./reducers/userReducers";
 import { userDetailsReducer } from "./reducers/userReducers";
 import { userUpdateProfileReducer } from "./reducers/userReducers";
+import { orderCreateReducer } from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -17,6 +18,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [];
